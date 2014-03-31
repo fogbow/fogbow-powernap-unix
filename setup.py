a15339a5eed839750b4a9c212e3a8133588a7041
Abmar Barros <abmargb@gmail.com>
2014-03-31 21:18:36 +0200

    Added setup.py bits

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'fogbow-opportunism',
+      version = '1.0',
+      description = 'Opportunism module for fogbow cloud',
+      author = 'Marcos Nobrega',
+      author_email = 'marcosancj@lsd.ufcg.edu.br',
+      url = 'https://github.com/fogbow/fogbow-opportunism',
+      packages = ['powernap'],
+      package_dir = {'powernap': 'powernap/powernap'},
+      scripts = ['powernap/sbin/powernapd']
+     )
