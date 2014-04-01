ca4e1cee2a8991a171119098dfe8083bfbc09627
marcosnobrega <marcosnobregajr@gmail.com>
2014-04-01 15:44:38 -0300

    Added start action script and renamed stop script

--- actions/openstack/stop-node.py
+++
@@ -1,8 +0,0 @@
-from novaclient.v1_1 import client
-nt = client.Client('admin', 'labstack', 'demo', 'http://150.165.85.110:5000/v2.0', service_type='compute')
-search_opts = {'host': 'openstack-VirtualBox'}
-server_list = nt.servers.list(search_opts=search_opts)
-for one_server in server_list:
-	one_server.force_delete()
-
-
