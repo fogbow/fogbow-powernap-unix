3575386ce89ac092dcf13491d84ed38a5cf68d0f
Abmar Barros <abmargb@gmail.com>
2014-10-09 13:32:16 -0300

    Updating version

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ for action_dir in os.listdir('actions'):
   data_files.append((os.path.join('actions', action_dir), action_scripts))
 
 setup(name = 'fogbow-opportunism',
-      version = '1.0',
+      version = '1.1',
       description = 'Opportunism module for fogbow cloud',
       author = 'Marcos Nobrega',
       author_email = 'marcosancj@lsd.ufcg.edu.br',
-      url = 'https://github.com/fogbow/fogbow-opportunism',
+      url = 'https://github.com/fogbow/fogbow-powernap-unix',
       packages = ['powernap', 'powernap.monitors'],
       package_dir = {'powernap': 'powernap/powernap'},
       scripts = ['powernap/sbin/powernapd'],
